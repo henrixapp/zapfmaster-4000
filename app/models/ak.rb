ae5a4315ec08f07e8fa7d885dc5b4ca7b1da1e9e
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-05-30 00:15:23 +0200

    update

--- app/models/ak.rb
+++ app/models/ak.rb
@@ -4,11 +4,12 @@ class Ak < ApplicationRecord
   has_many :responsibles
   has_many :persons, :through => :responsibles
   def self.from_js(js,ts)
-    Ak.create(timeslot:ts,room:Room.find_or_create_by(name:js["room"]),
+    ak= Ak.create(timeslot:ts,room:Room.find_or_create_by(name:js["room"]),
     name:js["name"],
      short_name:js["short"],
      url:js["url"])
-     person_reg= /*\(*)/
     p= Person.find_or_create_by(name:js["responsible"])
+    Responsible.create(ak:ak,person:p)
+    ak
   end
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,11 +1,21 @@
 class Timeslot < ApplicationRecord
     def self.from_js(js)
-        Timeslot.create(name:js["name"],
+        t= Timeslot.create(name:js["name"],
             begin_humanized:js["begin"],
         end_humanized:js["end"],
         ende:js["finished"],
         typus:js["type"],
         beginn:Time.at(js["timestamp"]),
         ende:js["finish"])
+        if not js["aks"].nil?
+        js["aks"].each do |ak|
+            Ak.from_js(ak,t)
+        end
+    end
+    end
+    def self.import(all)
+        all["slots"].each do |ts|
+            Timeslot.from_js(ts)
+        end
     end
 end
